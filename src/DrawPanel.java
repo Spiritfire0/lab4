bcad587c5b3e9687803fc63b6745d051b630dcfb
ViktorYeet <viktor@stefansbo.se>
2023-11-27 14:50:22 +0100

    Nothing left to do

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -11,7 +11,6 @@ import javax.swing.*;
 // This panel represent the animated part of the view with the car images.
 
 public class DrawPanel extends JPanel {
-    // Just a single image, TODO: Generalize
     HashMap<Car,BufferedImage> carToImage;
     // To keep track of a list of single cars position
     ArrayList<Car> cars;
@@ -35,8 +34,6 @@ public class DrawPanel extends JPanel {
         }
     }
 
-    // This method is called each time the panel updates/refreshes/repaints itself
-    // TODO: Change to suit your needs.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
